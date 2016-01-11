383091186243b7dfdb1fd35b16666a83cf13d3b6
briwestervelt <briwestervelt@gmail.com>
2016-01-11 10:58:14 -0500

    initial commit

---
+++ Config/js/main.js
@@ -0,0 +1,53 @@
+(function() {
+  loadOptions();
+  submitHandler();
+})();
+
+function submitHandler(){
+  var $submitButton = $("#submitButton")
+
+  $submitButton.on("click", function(){
+    console.log("submitted");
+
+    var return_to = getQueryParam('return_to', 'pebble.js://close#');
+    document.location = return_to + encodeURIComponent(JSON.stringify(getAndStoreConfigureData()));
+  });
+}
+
+function loadOptions() {
+  var $backgroundColorPicker = $("#backgroundColorPicker");
+  var $foregroundColorPicker = $("#foregroundColorPicker");
+
+  if (localStorage.backgroundColor) {
+    $backgroundColorPicker[0].value = localStorage.backgroundColor;
+    $foregroundColorPicker[0].value = localStorage.foregroundColor;
+  }
+}
+
+function getAndStoreConfigureData(){
+  var $backgroundColorPicker = $("#backgroundColorPicker");
+  var $foregroundColorPicker = $("#foregroundColorPicker");
+
+  var options = {
+    backgroundColor: $backgroundColorPicker.val(),
+    foregroundColor: $foregroundColorPicker.val()
+  }
+
+  localStorage.backgroundColor = options.backgroundColor;
+  localStorage.foregroundColor = options.foregroundColor;
+
+  console.log("Got options " + JSON.stringify(options));
+  return options;
+}
+
+function getQueryParam(variable, defaultValue) {
+  var query = location.search.substring(1);
+  var vars = query.split('&');
+  for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split('=');
+    if (pair[0] === variable) {
+      return decodeURIComponent(pair[1]);
+    }
+  }
+  return defaultValue || false;
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# pebble_config_test
+
+This is my first test at making a pebble configuration page
